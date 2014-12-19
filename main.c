906b43e29cf26691a217228a9213244633143d63
David Cohen <davelcohen2000@yahoo.com>
2014-12-19 06:00:17 -0500

    Added joystick update thread but I have to fix some weird error

--- main.c
+++ main.c
@@ -5,18 +5,36 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>      /* Errors */
+#include <pthread.h>	/* POSIX Threads */
 
 #include "joystick.h"
 
+#define BUFSIZE 1024
+
 typedef enum { false = 0, true = !false} bool;
+
+/* prototype for thread routine */
+void joystick_update ( void *ptr );
+
+/* struct to hold data to be passed to a thread
+this shows how multiple data items can be passed to a thread */
+typedef struct struct_thdata
+{
+    int thread_no;
+    char message[100];
+} thdata;
 
 /* a little test program */
 int main(int argc, char *argv[])
 {
+	char buf[BUFSIZE];
 	int joystick_x;
 	int joystick_y;
 	int joystick_z;
 	bool fire_pressed = false;
+	pthread_t joyThread;
+	thdata joyData; // this is a structure we can pass to the thread
 
 	// true only if an event we are using is activated
 	bool relevant = true;
@@ -26,6 +44,12 @@ int main(int argc, char *argv[])
 
 	struct js_event jse;
 
+	/* initialize data to pass to thread 1 */
+    joyData.thread_no = 1;
+    strcpy(joyData.message, "Joystick thread");
+	
+	pthread_create(&joyThread, NULL, (void *) &joystick_update, (void *) &joyData);
+	
 	fd = open_joystick(0);
 	if (fd < 0) {
 		printf("open failed.\n");
@@ -33,6 +57,18 @@ int main(int argc, char *argv[])
 	}
 
 	while (!done) {
+		/* get message line from the user */
+		printf("Please enter msg: ");
+		bzero(buf, BUFSIZE);
+		fgets(buf, BUFSIZE, stdin);
+
+		/*
+		 *Check to see if we're killing the client
+		 */
+		if(strcmp(buf, "99\n") == 0)
+		{
+			done = 1;
+  		}
 		rc = read_joystick_event(&jse);
 		usleep(1000);
 		if (rc == 1) {
@@ -67,3 +103,19 @@ int main(int argc, char *argv[])
 		}
 	}
 }
+
+
+/**
+ * Joystick update function
+ * it accepts a void pointer 
+**/
+void joystick_update ( void *ptr )
+{
+    thdata *jdata;            
+    jdata = (thdata *) ptr;  /* type cast to a pointer to thdata */
+    
+    /* do the work */
+    printf("Thread %d says %s \n", jdata->thread_no, jdata->message);
+    
+    pthread_exit(0); /* exit */
+} 
